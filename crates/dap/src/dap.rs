499b3777f7b9198f5a61b9530d0a40e79a4a6b4e
Kristoffer Grönlund <k@ziran.se>
2025-11-22 23:19:37 +0100

    Remove telemetry from dap

--- crates/dap/src/dap.rs
+++ crates/dap/src/dap.rs
@@ -38,39 +38,3 @@ pub async fn configure_tcp_connection(
 
     Ok((host, port, timeout))
 }
-
-#[derive(Clone, Copy, Serialize)]
-#[serde(rename_all = "snake_case")]
-pub enum TelemetrySpawnLocation {
-    Gutter,
-    ScenarioList,
-    Custom,
-}
-
-pub fn send_telemetry(scenario: &DebugScenario, location: TelemetrySpawnLocation, cx: &App) {
-    let Some(adapter) = cx.global::<DapRegistry>().adapter(&scenario.adapter) else {
-        return;
-    };
-    let dock = DebuggerSettings::get_global(cx).dock;
-    let config = scenario.config.clone();
-    let with_build_task = scenario.build.is_some();
-    let adapter_name = scenario.adapter.clone();
-    cx.spawn(async move |_| {
-        let kind = adapter
-            .request_kind(&config)
-            .await
-            .ok()
-            .map(serde_json::to_value)
-            .and_then(Result::ok);
-
-        telemetry::event!(
-            "Debugger Session Started",
-            spawn_location = location,
-            with_build_task = with_build_task,
-            kind = kind,
-            adapter = adapter_name,
-            dock_position = dock,
-        );
-    })
-    .detach();
-}
