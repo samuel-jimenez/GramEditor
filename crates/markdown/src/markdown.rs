abdae1eb373e93c1e9411c09d8dc54420d565893
tidely <43219534+tidely@users.noreply.github.com>
2025-12-17 09:56:05 +0200

    gpui: Remove intermediate allocations when reconstructing text from a `TextLayout` (#45037)
    Closes #ISSUE
    
    Remove some intermediate allocations when reconstructing text or wrapped
    text from a `TextLayout`. Currently creates a intermediate `Vec<String>`
    which gets joined, when you could join an `impl Iterator<Item = &str>`
    
    Release Notes:
    
    - N/A *or* Added/Fixed/Improved ...

--- crates/markdown/src/markdown.rs
+++ crates/markdown/src/markdown.rs
@@ -1943,7 +1943,7 @@ impl RenderedText {
     }
 
     fn text_for_range(&self, range: Range<usize>) -> String {
-        let mut ret = vec![];
+        let mut accumulator = String::new();
 
         for line in self.lines.iter() {
             if range.start > line.source_end {
@@ -1968,9 +1968,12 @@ impl RenderedText {
             }
             .min(text.len());
 
-            ret.push(text[start..end].to_string());
+            accumulator.push_str(&text[start..end]);
+            accumulator.push('\n');
         }
-        ret.join("\n")
+        // Remove trailing newline
+        accumulator.pop();
+        accumulator
     }
 
     fn link_for_position(&self, position: Point<Pixels>) -> Option<&RenderedLink> {
