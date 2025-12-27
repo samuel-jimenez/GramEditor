jonx/zed-aros

Enable configuring edit prediction providers through the settings UI (#44505)
- Edit prediction providers can now be configured through the settings
UI
- Cleaned up the status bar menu to only show _configured_ providers
- Added to the status bar icon button tooltip the name of the active
provider
- Only display the data collection functionality under "Privacy" for the
Zed models
- Moved the Codestral edit prediction provider out of the Mistral
section in the agent panel into the settings UI
- Refined and improved UI and states for configuring GitHub Copilot as
both an agent and edit prediction provider

#### Todos before merge:

- [x] UI: Unify with settings UI style and tidy it all up
- [x] Unify Copilot modal `impl`s to use separate window
- [x] Remove stop light icons from GitHub modal
- [x] Make dismiss events work on GitHub modal
- [ ] Investigate workarounds to tell if Copilot authenticated even when
LSP not running


Release Notes:

- settings_ui: Added a section for configuring edit prediction providers
under AI > Edit Predictions, including Codestral and GitHub Copilot.
Once you've updated you can use the following link to open it:
zed://settings/edit_predictions.providers

---------

Co-authored-by: Ben Kunkle <ben@zed.dev>

crates/ui/src/components/button/button_link.rs
@@ -1,0 +1,102 @@
+use gpui::{IntoElement, Window, prelude::*};
+
+use crate::{ButtonLike, prelude::*};
+
+/// A button that takes an underline to look like a regular web link.
+/// It also contains an arrow icon to communicate the link takes you out of Zed.
+///
+/// # Usage Example
+///
+/// ```
+/// use ui::ButtonLink;
+///
+/// let button_link = ButtonLink::new("Click me", "https://example.com");
+/// ```
+#[derive(IntoElement, RegisterComponent)]
+pub struct ButtonLink {
+    label: SharedString,
+    label_size: LabelSize,
+    label_color: Color,
+    link: String,
+    no_icon: bool,
+}
+
+impl ButtonLink {
+    pub fn new(label: impl Into<SharedString>, link: impl Into<String>) -> Self {
+        Self {
+            link: link.into(),
+            label: label.into(),
+            label_size: LabelSize::Default,
+            label_color: Color::Default,
+            no_icon: false,
+        }
+    }
+
+    pub fn no_icon(mut self, no_icon: bool) -> Self {
+        self.no_icon = no_icon;
+        self
+    }
+
+    pub fn label_size(mut self, label_size: LabelSize) -> Self {
+        self.label_size = label_size;
+        self
+    }
+
+    pub fn label_color(mut self, label_color: Color) -> Self {
+        self.label_color = label_color;
+        self
+    }
+}
+
+impl RenderOnce for ButtonLink {
+    fn render(self, _window: &mut Window, _cx: &mut App) -> impl IntoElement {
+        let id = format!("{}-{}", self.label, self.link);
+
+        ButtonLike::new(id)
+            .size(ButtonSize::None)
+            .child(
+                h_flex()
+                    .gap_0p5()
+                    .child(
+                        Label::new(self.label)
+                            .size(self.label_size)
+                            .color(self.label_color)
+                            .underline(),
+                    )
+                    .when(!self.no_icon, |this| {
+                        this.child(
+                            Icon::new(IconName::ArrowUpRight)
+                                .size(IconSize::Small)
+                                .color(Color::Muted),
+                        )
+                    }),
+            )
+            .on_click(move |_, _, cx| cx.open_url(&self.link))
+            .into_any_element()
+    }
+}
+
+impl Component for ButtonLink {
+    fn scope() -> ComponentScope {
+        ComponentScope::Navigation
+    }
+
+    fn description() -> Option<&'static str> {
+        Some("A button that opens a URL.")
+    }
+
+    fn preview(_window: &mut Window, _cx: &mut App) -> Option<AnyElement> {
+        Some(
+            v_flex()
+                .gap_6()
+                .child(
+                    example_group(vec![single_example(
+                        "Simple",
+                        ButtonLink::new("zed.dev", "https://zed.dev").into_any_element(),
+                    )])
+                    .vertical(),
+                )
+                .into_any_element(),
+        )
+    }
+}

crates/ui/src/components/inline_code.rs
@@ -1,0 +1,64 @@
+use crate::prelude::*;
+use gpui::{AnyElement, IntoElement, ParentElement, Styled};
+
+/// InlineCode mimics the way inline code is rendered when wrapped in backticks in Markdown.
+///
+/// # Usage Example
+///
+/// ```
+/// use ui::InlineCode;
+///
+/// let InlineCode = InlineCode::new("<div>hey</div>");
+/// ```
+#[derive(IntoElement, RegisterComponent)]
+pub struct InlineCode {
+    label: SharedString,
+    label_size: LabelSize,
+}
+
+impl InlineCode {
+    pub fn new(label: impl Into<SharedString>) -> Self {
+        Self {
+            label: label.into(),
+            label_size: LabelSize::Default,
+        }
+    }
+
+    /// Sets the size of the label.
+    pub fn label_size(mut self, size: LabelSize) -> Self {
+        self.label_size = size;
+        self
+    }
+}
+
+impl RenderOnce for InlineCode {
+    fn render(self, _: &mut Window, cx: &mut App) -> impl IntoElement {
+        h_flex()
+            .min_w_0()
+            .px_0p5()
+            .overflow_hidden()
+            .bg(cx.theme().colors().text.opacity(0.05))
+            .child(Label::new(self.label).size(self.label_size).buffer_font(cx))
+    }
+}
+
+impl Component for InlineCode {
+    fn scope() -> ComponentScope {
+        ComponentScope::DataDisplay
+    }
+
+    fn preview(_window: &mut Window, _cx: &mut App) -> Option<AnyElement> {
+        Some(
+            v_flex()
+                .gap_6()
+                .child(
+                    example_group(vec![single_example(
+                        "Simple",
+                        InlineCode::new("zed.dev").into_any_element(),
+                    )])
+                    .vertical(),
+                )
+                .into_any_element(),
+        )
+    }
+}

crates/ui/src/components/list/list_bullet_item.rs
@@ -1,17 +1,32 @@
-use crate::{ListItem, prelude::*};
-use component::{Component, ComponentScope, example_group_with_title, single_example};
+use crate::{ButtonLink, ListItem, prelude::*};
+use component::{Component, ComponentScope, example_group, single_example};
 use gpui::{IntoElement, ParentElement, SharedString};
 
 #[derive(IntoElement, RegisterComponent)]
 pub struct ListBulletItem {
     label: SharedString,
+    label_color: Option<Color>,
+    children: Vec<AnyElement>,
 }
 
 impl ListBulletItem {
     pub fn new(label: impl Into<SharedString>) -> Self {
         Self {
             label: label.into(),
+            label_color: None,
+            children: Vec::new(),
         }
+    }
+
+    pub fn label_color(mut self, color: Color) -> Self {
+        self.label_color = Some(color);
+        self
+    }
+}
+
+impl ParentElement for ListBulletItem {
+    fn extend(&mut self, elements: impl IntoIterator<Item = AnyElement>) {
+        self.children.extend(elements)
     }
 }
 
@@ -34,7 +49,18 @@ impl RenderOnce for ListBulletItem {
                                 .color(Color::Hidden),
                         ),
                     )
-                    .child(div().w_full().min_w_0().child(Label::new(self.label))),
+                    .map(|this| {
+                        if !self.children.is_empty() {
+                            this.child(h_flex().gap_0p5().flex_wrap().children(self.children))
+                        } else {
+                            this.child(
+                                div().w_full().min_w_0().child(
+                                    Label::new(self.label)
+                                        .color(self.label_color.unwrap_or(Color::Default)),
+                                ),
+                            )
+                        }
+                    }),
             )
             .into_any_element()
     }
@@ -46,37 +72,43 @@ impl Component for ListBulletItem {
     }
 
     fn description() -> Option<&'static str> {
-        Some("A list item with a bullet point indicator for unordered lists.")
+        Some("A list item with a dash indicator for unordered lists.")
     }
 
     fn preview(_window: &mut Window, _cx: &mut App) -> Option<AnyElement> {
+        let basic_examples = vec![
+            single_example(
+                "Simple",
+                ListBulletItem::new("First bullet item").into_any_element(),
+            ),
+            single_example(
+                "Multiple Lines",
+                v_flex()
+                    .child(ListBulletItem::new("First item"))
+                    .child(ListBulletItem::new("Second item"))
+                    .child(ListBulletItem::new("Third item"))
+                    .into_any_element(),
+            ),
+            single_example(
+                "Long Text",
+                ListBulletItem::new(
+                    "A longer bullet item that demonstrates text wrapping behavior",
+                )
+                .into_any_element(),
+            ),
+            single_example(
+                "With Link",
+                ListBulletItem::new("")
+                    .child(Label::new("Create a Zed account by"))
+                    .child(ButtonLink::new("visiting the website", "https://zed.dev"))
+                    .into_any_element(),
+            ),
+        ];
+
         Some(
             v_flex()
                 .gap_6()
-                .child(example_group_with_title(
-                    "Bullet Items",
-                    vec![
-                        single_example(
-                            "Simple",
-                            ListBulletItem::new("First bullet item").into_any_element(),
-                        ),
-                        single_example(
-                            "Multiple Lines",
-                            v_flex()
-                                .child(ListBulletItem::new("First item"))
-                                .child(ListBulletItem::new("Second item"))
-                                .child(ListBulletItem::new("Third item"))
-                                .into_any_element(),
-                        ),
-                        single_example(
-                            "Long Text",
-                            ListBulletItem::new(
-                                "A longer bullet item that demonstrates text wrapping behavior",
-                            )
-                            .into_any_element(),
-                        ),
-                    ],
-                ))
+                .child(example_group(basic_examples).vertical())
                 .into_any_element(),
         )
     }

samuel-jimenez/GramEditor

crates/ui/src/components/button/button_link.rs
@@ -1,0 +1,102 @@
+use gpui::{IntoElement, Window, prelude::*};
+
+use crate::{ButtonLike, prelude::*};
+
+/// A button that takes an underline to look like a regular web link.
+/// It also contains an arrow icon to communicate the link takes you out of Gram.
+///
+/// # Usage Example
+///
+/// ```
+/// use ui::ButtonLink;
+///
+/// let button_link = ButtonLink::new("Click me", "https://example.com");
+/// ```
+#[derive(IntoElement, RegisterComponent)]
+pub struct ButtonLink {
+    label: SharedString,
+    label_size: LabelSize,
+    label_color: Color,
+    link: String,
+    no_icon: bool,
+}
+
+impl ButtonLink {
+    pub fn new(label: impl Into<SharedString>, link: impl Into<String>) -> Self {
+        Self {
+            link: link.into(),
+            label: label.into(),
+            label_size: LabelSize::Default,
+            label_color: Color::Default,
+            no_icon: false,
+        }
+    }
+
+    pub fn no_icon(mut self, no_icon: bool) -> Self {
+        self.no_icon = no_icon;
+        self
+    }
+
+    pub fn label_size(mut self, label_size: LabelSize) -> Self {
+        self.label_size = label_size;
+        self
+    }
+
+    pub fn label_color(mut self, label_color: Color) -> Self {
+        self.label_color = label_color;
+        self
+    }
+}
+
+impl RenderOnce for ButtonLink {
+    fn render(self, _window: &mut Window, _cx: &mut App) -> impl IntoElement {
+        let id = format!("{}-{}", self.label, self.link);
+
+        ButtonLike::new(id)
+            .size(ButtonSize::None)
+            .child(
+                h_flex()
+                    .gap_0p5()
+                    .child(
+                        Label::new(self.label)
+                            .size(self.label_size)
+                            .color(self.label_color)
+                            .underline(),
+                    )
+                    .when(!self.no_icon, |this| {
+                        this.child(
+                            Icon::new(IconName::ArrowUpRight)
+                                .size(IconSize::Small)
+                                .color(Color::Muted),
+                        )
+                    }),
+            )
+            .on_click(move |_, _, cx| cx.open_url(&self.link))
+            .into_any_element()
+    }
+}
+
+impl Component for ButtonLink {
+    fn scope() -> ComponentScope {
+        ComponentScope::Navigation
+    }
+
+    fn description() -> Option<&'static str> {
+        Some("A button that opens a URL.")
+    }
+
+    fn preview(_window: &mut Window, _cx: &mut App) -> Option<AnyElement> {
+        Some(
+            v_flex()
+                .gap_6()
+                .child(
+                    example_group(vec![single_example(
+                        "Simple",
+                        ButtonLink::new("codeberg.org", "https://codeberg.org").into_any_element(),
+                    )])
+                    .vertical(),
+                )
+                .into_any_element(),
+        )
+    }
+}

crates/ui/src/components/inline_code.rs
@@ -1,0 +1,64 @@
+use crate::prelude::*;
+use gpui::{AnyElement, IntoElement, ParentElement, Styled};
+
+/// InlineCode mimics the way inline code is rendered when wrapped in backticks in Markdown.
+///
+/// # Usage Example
+///
+/// ```
+/// use ui::InlineCode;
+///
+/// let InlineCode = InlineCode::new("<div>hey</div>");
+/// ```
+#[derive(IntoElement, RegisterComponent)]
+pub struct InlineCode {
+    label: SharedString,
+    label_size: LabelSize,
+}
+
+impl InlineCode {
+    pub fn new(label: impl Into<SharedString>) -> Self {
+        Self {
+            label: label.into(),
+            label_size: LabelSize::Default,
+        }
+    }
+
+    /// Sets the size of the label.
+    pub fn label_size(mut self, size: LabelSize) -> Self {
+        self.label_size = size;
+        self
+    }
+}
+
+impl RenderOnce for InlineCode {
+    fn render(self, _: &mut Window, cx: &mut App) -> impl IntoElement {
+        h_flex()
+            .min_w_0()
+            .px_0p5()
+            .overflow_hidden()
+            .bg(cx.theme().colors().text.opacity(0.05))
+            .child(Label::new(self.label).size(self.label_size).buffer_font(cx))
+    }
+}
+
+impl Component for InlineCode {
+    fn scope() -> ComponentScope {
+        ComponentScope::DataDisplay
+    }
+
+    fn preview(_window: &mut Window, _cx: &mut App) -> Option<AnyElement> {
+        Some(
+            v_flex()
+                .gap_6()
+                .child(
+                    example_group(vec![single_example(
+                        "Simple",
+                        InlineCode::new("codeberg.org").into_any_element(),
+                    )])
+                    .vertical(),
+                )
+                .into_any_element(),
+        )
+    }
+}

crates/ui/src/components/list/list_bullet_item.rs
@@ -1,17 +1,32 @@
-use crate::{ListItem, prelude::*};
-use component::{Component, ComponentScope, example_group_with_title, single_example};
+use crate::{ButtonLink, ListItem, prelude::*};
+use component::{Component, ComponentScope, example_group, single_example};
 use gpui::{IntoElement, ParentElement, SharedString};
 
 #[derive(IntoElement, RegisterComponent)]
 pub struct ListBulletItem {
     label: SharedString,
+    label_color: Option<Color>,
+    children: Vec<AnyElement>,
 }
 
 impl ListBulletItem {
     pub fn new(label: impl Into<SharedString>) -> Self {
         Self {
             label: label.into(),
+            label_color: None,
+            children: Vec::new(),
         }
+    }
+
+    pub fn label_color(mut self, color: Color) -> Self {
+        self.label_color = Some(color);
+        self
+    }
+}
+
+impl ParentElement for ListBulletItem {
+    fn extend(&mut self, elements: impl IntoIterator<Item = AnyElement>) {
+        self.children.extend(elements)
     }
 }
 
@@ -34,7 +49,18 @@ impl RenderOnce for ListBulletItem {
                                 .color(Color::Hidden),
                         ),
                     )
-                    .child(div().w_full().min_w_0().child(Label::new(self.label))),
+                    .map(|this| {
+                        if !self.children.is_empty() {
+                            this.child(h_flex().gap_0p5().flex_wrap().children(self.children))
+                        } else {
+                            this.child(
+                                div().w_full().min_w_0().child(
+                                    Label::new(self.label)
+                                        .color(self.label_color.unwrap_or(Color::Default)),
+                                ),
+                            )
+                        }
+                    }),
             )
             .into_any_element()
     }
@@ -46,37 +72,46 @@ impl Component for ListBulletItem {
     }
 
     fn description() -> Option<&'static str> {
-        Some("A list item with a bullet point indicator for unordered lists.")
+        Some("A list item with a dash indicator for unordered lists.")
     }
 
     fn preview(_window: &mut Window, _cx: &mut App) -> Option<AnyElement> {
+        let basic_examples = vec![
+            single_example(
+                "Simple",
+                ListBulletItem::new("First bullet item").into_any_element(),
+            ),
+            single_example(
+                "Multiple Lines",
+                v_flex()
+                    .child(ListBulletItem::new("First item"))
+                    .child(ListBulletItem::new("Second item"))
+                    .child(ListBulletItem::new("Third item"))
+                    .into_any_element(),
+            ),
+            single_example(
+                "Long Text",
+                ListBulletItem::new(
+                    "A longer bullet item that demonstrates text wrapping behavior",
+                )
+                .into_any_element(),
+            ),
+            single_example(
+                "With Link",
+                ListBulletItem::new("")
+                    .child(Label::new("Create a Codeberg account by"))
+                    .child(ButtonLink::new(
+                        "visiting the website",
+                        "https://codeberg.org",
+                    ))
+                    .into_any_element(),
+            ),
+        ];
+
         Some(
             v_flex()
                 .gap_6()
-                .child(example_group_with_title(
-                    "Bullet Items",
-                    vec![
-                        single_example(
-                            "Simple",
-                            ListBulletItem::new("First bullet item").into_any_element(),
-                        ),
-                        single_example(
-                            "Multiple Lines",
-                            v_flex()
-                                .child(ListBulletItem::new("First item"))
-                                .child(ListBulletItem::new("Second item"))
-                                .child(ListBulletItem::new("Third item"))
-                                .into_any_element(),
-                        ),
-                        single_example(
-                            "Long Text",
-                            ListBulletItem::new(
-                                "A longer bullet item that demonstrates text wrapping behavior",
-                            )
-                            .into_any_element(),
-                        ),
-                    ],
-                ))
+                .child(example_group(basic_examples).vertical())
                 .into_any_element(),
         )
     }