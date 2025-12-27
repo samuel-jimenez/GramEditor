856920544ac4bbc867983b796c02ad3b6dd87143
Finn Evers <finn@zed.dev>
2025-12-27 17:26:08 +0100

    editor: Fix active line number regressions with relative counting (#45741)
    Follow-up to https://github.com/zed-industries/zed/pull/45164 which
    caused the active line number to always be `0` instead of the actual
    current line number. No release notes since its only on nightly
    
    Release Notes:
    
    - N/A

--- crates/editor/src/editor_tests.rs
+++ crates/editor/src/editor_tests.rs
@@ -28313,7 +28313,7 @@ fn test_relative_line_numbers(cx: &mut TestAppContext) {
             assert_eq!(
                 relative_number,
                 snapshot
-                    .relative_line_delta(display_row, base_display_row)
+                    .relative_line_delta(display_row, base_display_row, false)
                     .unsigned_abs() as u32,
             );
         }
@@ -28323,6 +28323,7 @@ fn test_relative_line_numbers(cx: &mut TestAppContext) {
             .into_iter()
             .enumerate()
             .map(|(i, row)| (DisplayRow(row), i.abs_diff(wrapped_base_row) as u32))
+            .filter(|(row, _)| *row != base_display_row)
             .collect_vec();
         let actual_relative_numbers = snapshot
             .calculate_relative_line_numbers(
@@ -28339,7 +28340,7 @@ fn test_relative_line_numbers(cx: &mut TestAppContext) {
             assert_eq!(
                 relative_number,
                 snapshot
-                    .relative_wrapped_line_delta(display_row, base_display_row)
+                    .relative_line_delta(display_row, base_display_row, true)
                     .unsigned_abs() as u32,
             );
         }
