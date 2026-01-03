0c9fccda1f451c0da4b5981f438efa7c7657484e
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-01-03 22:39:49 +0100

    editor: Use a default font for the minimap (#45999)
    Closes #45496
    Should help #43460
    And probably also #44503
    
    The minimap usually renders about 10x more lines of code and shaped text
    than the editor. With expensive fonts such as Google Sans Code, this can
    cause noticeable lag, since we attempt to synthesize 300+ lines of code
    using a heavy font. Because minimap text is rendered at around 2px and
    is essentially illegible, it doesn’t make sense to use the custom buffer
    font.
    
    Release Notes:
    
    - Improved minimap performance when using custom fonts
    
    ---------
    
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -18042,6 +18042,7 @@ impl Editor {
         cx: &mut Context<Self>,
     ) -> Entity<Self> {
         const MINIMAP_FONT_WEIGHT: gpui::FontWeight = gpui::FontWeight::BLACK;
+        const MINIMAP_FONT_FAMILY: SharedString = SharedString::new_static(".GramMono");
 
         let mut minimap = Editor::new_internal(
             EditorMode::Minimap {
@@ -18057,6 +18058,7 @@ impl Editor {
         minimap.set_text_style_refinement(TextStyleRefinement {
             font_size: Some(MINIMAP_FONT_SIZE),
             font_weight: Some(MINIMAP_FONT_WEIGHT),
+            font_family: Some(MINIMAP_FONT_FAMILY),
             ..Default::default()
         });
         minimap.update_minimap_configuration(minimap_settings, cx);
