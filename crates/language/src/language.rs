82568910b1932cc90ae3dc3d62f8854ba9830421
Kristoffer Grönlund <k@ziran.se>
2026-01-09 00:29:29 +0100

    Reformat comment to be more readable

--- crates/language/src/language.rs
+++ crates/language/src/language.rs
@@ -1,11 +1,23 @@
-//! The `language` crate provides a large chunk of Gram's language-related
-//! features (the other big contributors being project and lsp crates that revolve around LSP features).
-//! Namely, this crate:
-//! - Provides [`Language`], [`Grammar`] and [`LanguageRegistry`] types that
-//!   use Tree-sitter to provide syntax highlighting to the editor; note though that `language` doesn't perform the highlighting by itself. It only maps ranges in a buffer to colors. Treesitter is also used for buffer outlines (lists of symbols in a buffer)
-//! - Exposes [`LanguageConfig`] that describes how constructs (like brackets or line comments) should be handled by the editor for a source file of a particular language.
+//! The `language` crate provides a large chunk of language-related features
+//! (the other big contributors being project and lsp crates that revolve around
+//! LSP features).
 //!
-//! Notably we do *not* assign a single language to a single file; in real world a single file can consist of multiple programming languages - HTML is a good example of that - and `language` crate tends to reflect that status quo in its API.
+//! This crate:
+//!
+//! - Provides [`Language`], [`Grammar`] and [`LanguageRegistry`] types that
+//!   use Tree-sitter to provide syntax highlighting to the editor; note though
+//!   that `language` doesn't perform the highlighting by itself. It only maps
+//!   ranges in a buffer to colors. Treesitter is also used for buffer outlines
+//!   (lists of symbols in a buffer)
+//!
+//! - Exposes [`LanguageConfig`] that describes how constructs (like brackets or
+//!   line comments) should be handled by the editor for a source file of a
+//!   particular language.
+//!
+//! Notably we do *not* assign a single language to a single file; in real world
+//! a single file can consist of multiple programming languages - HTML is a good
+//! example of that - and `language` crate tends to reflect that status quo in
+//! its API.
 mod buffer;
 mod diagnostic_set;
 mod highlight_map;
