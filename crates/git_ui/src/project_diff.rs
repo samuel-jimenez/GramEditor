896e3fbfee35af2482712540bdb09f94fa15e83c
Cameron Mcloughlin <cameron.studdstreet@gmail.com>
2026-02-05 23:45:01 +0000

    git: Side-by-side diff searching (#48539)
    Searching (and related vim stuff like `*`/`n`/`N`) now work in the LHS
    of a
    split diff.
    
    Also fixes the bug with indent guides being visible through the spacer
    checkerboard pattern.
    
    Release Notes:
    
    - N/A

--- crates/git_ui/src/project_diff.rs
+++ crates/git_ui/src/project_diff.rs
@@ -779,9 +779,8 @@ impl Item for ProjectDiff {
         }
     }
 
-    fn as_searchable(&self, _: &Entity<Self>, cx: &App) -> Option<Box<dyn SearchableItemHandle>> {
-        // TODO(split-diff) SplitEditor should be searchable
-        Some(Box::new(self.editor.read(cx).rhs_editor().clone()))
+    fn as_searchable(&self, _: &Entity<Self>, _cx: &App) -> Option<Box<dyn SearchableItemHandle>> {
+        Some(Box::new(self.editor.clone()))
     }
 
     fn for_each_project_item(
